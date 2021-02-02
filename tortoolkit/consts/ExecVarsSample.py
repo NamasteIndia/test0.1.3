50d4b06617a31e88a523a693073efca3b9b132c6
NamasteIndia <49607060+NamasteIndia@users.noreply.github.com>
2021-02-02 14:52:17 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -2,33 +2,43 @@ try:
     from .ExecVars import ExecVars
 except:
     class ExecVars:
-        # TODO optimize for vps use fully - currently only heroku is focused
-        # Set true if its VPS [currently not fully working]
+        # Set true if its VPS
         IS_VPS = False
+        
         API_HASH = ""
-        API_ID = ""
+        API_ID = 0
         BOT_TOKEN = ""
         BASE_URL_OF_BOT = ""
+
+        # Edit the server port if you want to keep it default though.
+        SERVPORT = 80
+
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [1147526472]
+        ALD_USR = []
         
+        # Google Drive Index Link should include the base dir also See readme for more info
+        GD_INDEX_URL = False
+
         # Time to wait before edit message
         EDIT_SLEEP_SECS = 40
 
         # Telegram Upload Limit (in bytes)
-        TG_UP_LIMIT = 1800000000
+        TG_UP_LIMIT = 1700000000
 
         # Should force evething uploaded into Document
         FORCE_DOCUMENTS = False
 
         # Chracter to use as a completed progress 
-        COMPLETED_STR = "🌑"
+        COMPLETED_STR = "▰"
 
         # Chracter to use as a incomplete progress
-        REMAINING_STR = "⚪"
+        REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = ""
+        DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+        
+        # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
+        #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
         
         # The base direcory to which the files will be upload if using RCLONE
         RCLONE_BASE_DIR = "/"
@@ -50,11 +60,11 @@ except:
         # Name of the RCLONE drive from the config
         DEF_RCLONE_DRIVE = ""
 
-        # Max size of the videos in playlist allowed
-        MAX_YTPLAYLIST_SIZE = 200
+        # Max size of a playlist that is allowed (Number of videos)
+        MAX_YTPLAYLIST_SIZE = 20
         
         # Max size of the torrent allowed
-        MAX_TORRENT_SIZE = 30
+        MAX_TORRENT_SIZE = 10
 
         # This is to stop someone from abusing the system by imposing the limit
         # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
